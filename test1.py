3e6bd7a7a5f2ecd4ad709b7e42471ab971f33b47
yessol9096 <yessol9096@naver.com>
2017-05-25 02:05:30 +0900

    openapi불러오기

--- test1.py
+++ test1.py
@@ -1,18 +1,20 @@
-import urllib.request
+from urllib import request
+from xml.dom.minidom import *
+from xml.etree import ElementTree
+
+serviceKey = 'Lg8o2pi2x5AWT0RIJ5XkNewd%2BtSXe63rr5za624JyZiO1TiQpbdNmDhonlj1zFMucI35WMs0idy66DCXSgi%2FvA%3D%3D'
 
 
-import http.client
-
-def urlencode(string):
-    print("URLEncoding:",urllib.quote(string))
-
-url = 'http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown'
-queryParams = '?' + urllib.parse.urlencode({ urllib.parse.quote_plus('ServiceKey') : 'Lg8o2pi2x5AWT0RIJ5XkNewd%2BtSXe63rr5za624JyZiO1TiQpbdNmDhonlj1zFMucI35WMs0idy66DCXSgi%2FvA%3D%3D', urllib.parse.quote_plus('ServiceKey') : '-', urllib.parse.quote_plus('pageNo') : '1', urllib.parse.quote_plus('numOfRows') : '10' })
-
-request = urllib.request.Request(url + queryParams)
-request.get_method = lambda: 'GET'
-response_body = urllib.request.urlopen(request).read()
-print(response_body)
+response = request.urlopen('http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?'
+                                'serviceKey=Lg8o2pi2x5AWT0RIJ5XkNewd%2BtSXe63rr5za624JyZiO1TiQpbdNmDhonlj1zFMucI35WMs0idy66DCXSgi%2FvA%3D%3D'
+                                '&numOfRows=100&pageSize=100').read()
+tree = ElementTree.fromstring(response)
+    #print(response)
+itemElements = tree.getiterator("item")
+    #print(itemElements)
+for item in itemElements:
+    age = item.find("dutyName")
+    print(age.text)
 
 
 
